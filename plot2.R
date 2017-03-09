7b2414d3573db20886362ee0803ed9d68a3aec17
drexelhub <sethshome@gmail.com>
2017-03-09 12:13:20 -0500

    Cleaned up code

--- plot2.R
+++ plot2.R
@@ -1,13 +1,18 @@
 buildPlot2 <- function()
     {
         library(data.table)
+    
         df_full <- fread("household_power_consumption.txt", sep=";", colClasses=c("character"))
+        
         df_subset <- subset(df_full, Date=='1/2/2007' | Date=='2/2/2007')[,c("Date", "Time", "Global_active_power")]
         rm(df_full)
+        
         df_subset$DateTime <- as.POSIXct(strptime(paste(df_subset$Date, df_subset$Time, sep=" "), format="%d/%m/%Y %T"))
-        df_subset$Global_active_power <- as.numeric(df_subset$Global_active_power)
+
         png("plot2.png", width=480, height=480)
+        
         plot(df_subset$DateTime, df_subset$Global_active_power, axes=T, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+        
         dev.off();
 
     }
--- plot3.R
+++ plot3.R
@@ -1,15 +1,20 @@
 buildPlot3 <- function()
 {
     library(data.table)
+    
     df_full <- fread("household_power_consumption.txt", sep=";", colClasses=c("character"))
+    
     df_subset <- subset(df_full, Date=='1/2/2007' | Date=='2/2/2007')
     rm(df_full)
     df_subset$DateTime <- as.POSIXct(strptime(paste(df_subset$Date, df_subset$Time, sep=" "), format="%d/%m/%Y %T"))
+    
     png("plot3.png", width=480, height=480)
+    
     plot(df_subset$DateTime, df_subset$Sub_metering_1, axes=T, type="l", xlab="", ylab="Energy sub metering")
     lines(df_subset$DateTime, df_subset$Sub_metering_2, col="red")
     lines(df_subset$DateTime, df_subset$Sub_metering_3, col="blue")
     legend("topright", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col=c("black", "red", "blue"), lty=c(1,1,1))
+    
     dev.off();
     
 }
